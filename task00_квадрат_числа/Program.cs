14902eaa03b4bd8595310e6f626d45a652d1405c
deligtfullady <94959424+deligtfullady@users.noreply.github.com>
2023-10-08 02:14:40 +0300

    C#

---
+++ task00_квадрат_числа/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+0. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+разбор
+*/
+
+System.Console.WriteLine("Введите число: "); // cw - быстрый вызов Console.WriteLine
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber * userNumber;
+
+Console.WriteLine(result);
+Console.WriteLine(userNumber * userNumber);
