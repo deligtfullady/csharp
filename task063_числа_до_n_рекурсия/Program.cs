9e24e3a858b0e834ae3600c6f5e802ae7dcccf8a
deligtfullady <94959424+deligtfullady@users.noreply.github.com>
2023-11-17 00:02:39 +0300

    домашка

---
+++ task063_числа_до_n_рекурсия/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите три числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void intN(int number, int current = 1)
+{
+    while (current <= number)
+    System.Console.WriteLine(current + " ");
+    intN(number, current + 1);
+}
